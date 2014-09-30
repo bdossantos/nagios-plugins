d5a2bf1b73671a953c6752322ed68a45f2092777
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-09-30 14:05:16 +0200

    Add "memory error" pattern

--- check_dmesg.sh
+++ check_dmesg.sh
@@ -4,7 +4,7 @@ output=$(dmesg -T -l err,crit,alert,emerg 2>/dev/null || dmesg || exit 3)
 if test ! -z "$output"; then
   filtered_output=$({
     echo $output | egrep -o -i \
-    'Hardware Error|I/O error|hard resetting link|DRDY ERR|Out of memory|Killed process|temperature above threshold|Possible SYN flooding|segfault'
+    'Hardware Error|I/O error|hard resetting link|DRDY ERR|Out of memory|Killed process|temperature above threshold|Possible SYN flooding|segfault|MEMORY ERROR'
   })
 
   if test ! -z "$filtered_output"; then
