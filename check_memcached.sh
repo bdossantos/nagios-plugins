ac9d83e6e67175404965a86b2650ed3f02a5883d
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-10-30 18:55:36 +0100

    Coding style

--- check_memcached.sh
+++ check_memcached.sh
@@ -20,7 +20,7 @@
 # https://github.com/bdossantos/nagios-plugins
 #
 
-while test -n "$1"; do
+while [[ -n "$1" ]]; do
   case $1 in
     --host|-h)
       host=$2
@@ -51,7 +51,7 @@ port=${port:=11211}
 warn=${warn:=90}
 crit=${crit:=95}
 
-output=$((echo 'stats'; echo 'quit';) | nc "$host" "$port")
+output=$( (echo 'stats'; echo 'quit';) | nc "$host" "$port")
 
 if test $? -ne 0; then
   echo "CRITICAL - timed out connecting to memcached on ${host}:${port}"
@@ -61,18 +61,18 @@ fi
 limit_maxbytes=$(echo "$output" | grep 'limit_maxbytes' | awk '{ gsub(/\r/, ""); print $3 }')
 bytes=$(echo "$output" | grep ' bytes ' | awk '{ gsub(/\r/, ""); print $3 }')
 
-if test $bytes -eq 0; then
+if [[ "$bytes" -eq 0 ]]; then
   echo 'OK - Memcached is empty'
   exit 0
 fi
 
-used=$(expr $bytes \* 100 \/ $limit_maxbytes)
+used=$((bytes * 100 / limit_maxbytes))
 status="${used}% used (${bytes} of ${limit_maxbytes}) bytes used";
 
-if test $used -gt $crit; then
+if [[ $used -gt $crit ]]; then
   echo "CRITICAL - ${status}"
   exit 2
-elif test $used -gt $warn; then
+elif [[ $used -gt $warn ]]; then
   echo "WARNING - ${status}"
   exit 1
 else
