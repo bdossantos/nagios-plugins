fe002283f6ecb57799267d8da8aee35585b807c5
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-10-31 00:49:57 +0100

    Add help flag

--- check_application_health.sh
+++ check_application_health.sh
@@ -2,20 +2,15 @@
 #
 # Check application health
 #
-# eg : check_application_health.sh \
+# Usage: check_application_health.sh [-w webroot] [-c command] [-t timeout]
+#     -w, --webroot         Path to webroot
+#     -c, --command         Healthcheck command
+#     -t, --timeout         timeout, eg: 10s
+#     -h, --help            Display this screen
+#
+# eg: check_application_health.sh \
 #         -w '/var/www/myapp.io/current' \
 #         -c 'php app/console monitor:health'
-#
-# Options :
-#
-#   -w/--webroot)
-#       Path to webroot
-#
-#   -c/--command)
-#       Healthcheck command
-#
-#   -t/--timeout)
-#       timeout, eg: 10s
 #
 # (c) 2014, Benjamin Dos Santos <benjamin.dossantos@gmail.com>
 # https://github.com/bdossantos/nagios-plugins
@@ -31,6 +26,15 @@ while [[ -n "$1" ]]; do
       ;;
     -t | --timeout)
       timeout=$2
+      ;;
+    --help | -h)
+      sed -n '2,10p' "$0" | tr -d '#'
+      exit 0
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exec "$0" --help
+      exit 3
       ;;
   esac
   shift
