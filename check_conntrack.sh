96260ea92d13a31e4576cf42a121d2b1f40de4db
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-10-31 10:34:31 +0100

    Update documentation and add help flag

--- check_conntrack.sh
+++ check_conntrack.sh
@@ -2,16 +2,40 @@
 #
 # Check conntrack table usage
 #
+# Usage: check_conntrack.sh [-w warning] [-c critical]
+#     -w, --warning WARNING         Warning value (percent)
+#     -c, --critical CRITICAL       Critical value (percent)
+#     -h, --help                    Display this screen
+#
 # (c) 2014, Benjamin Dos Santos <benjamin.dossantos@gmail.com>
 # https://github.com/bdossantos/nagios-plugins
 #
 
-if [[ $# != 2 ]]; then
-  echo "Syntax: check_conntrack.sh <warn percent> <crit percent>"
-  echo
-  echo "Example: check_conntrack.sh 75 90"
-  exit 3
-fi
+while [[ -n "$1" ]]; do
+  case $1 in
+    --warning|-w)
+      warn=$2
+      shift
+      ;;
+    --critical|-c)
+      crit=$2
+      shift
+      ;;
+    --help|-h)
+      sed -n '2,9p' "$0" | tr -d '#'
+      exit 0
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exec "$0" --help
+      exit 3
+      ;;
+  esac
+  shift
+done
+
+warn=${warn:=90}
+crit=${crit:=95}
 
 conntrack_count=$(find /proc/sys -type f -name '*conntrack_count' | head -n 1)
 max_value=$(find /proc/sys -name '*conntrack_max' | head -n 1)
@@ -22,8 +46,8 @@ fi
 
 conntrack_count=$(head -n 1 < "$conntrack_count")
 max_value=$(head -n 1 < "$max_value")
-warn=$((max_value * $1 / 100))
-crit=$((max_value * $2 / 100))
+warn=$((max_value * warn / 100))
+crit=$((max_value * crit / 100))
 performance_data="conntrack_table=$conntrack_count;$warn;$crit;0;$max_value"
 
 if [[ $conntrack_count -gt $warn ]]; then
