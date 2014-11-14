4f4884b31c0c9eb4895fc58a9e70142efe12690a
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-11-15 00:03:51 +0100

    Add documenation and add `--help` flag

--- check_last.sh
+++ check_last.sh
@@ -2,9 +2,27 @@
 #
 # Return last logged in user
 #
+# Usage: check_last.sh
+#     -h, --help            Display this screen
+#
 # (c) 2014, Benjamin Dos Santos <benjamin.dossantos@gmail.com>
 # https://github.com/bdossantos/nagios-plugins
 #
+
+while [[ -n "$1" ]]; do
+  case $1 in
+    --help | -h)
+      sed -n '2,7p' "$0" | tr -d '#'
+      exit 3
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exec "$0" --help
+      exit 3
+      ;;
+  esac
+  shift
+done
 
 last -i | tac | tail -n 1
 exit 0
