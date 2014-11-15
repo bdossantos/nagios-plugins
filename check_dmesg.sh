28ab1808e724c1c64a5186adf5af9fc6b5c29297
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-11-15 01:23:58 +0100

    Add documenation and add `--help` flag

--- check_dmesg.sh
+++ check_dmesg.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 #
-# Check dmesg output
+# Check dmesg output for common errors
+#
+# Usage: check_dmesg.sh
+#     -h, --help            Display this screen
 #
 # (c) 2014, Benjamin Dos Santos <benjamin.dossantos@gmail.com>
 # https://github.com/bdossantos/nagios-plugins
 #
+
+while [[ -n "$1" ]]; do
+  case $1 in
+    --help | -h)
+      sed -n '2,7p' "$0" | tr -d '#'
+      exit 3
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exec "$0" --help
+      exit 3
+      ;;
+  esac
+  shift
+done
 
 output=$(dmesg -T -l err,crit,alert,emerg 2>/dev/null || dmesg || exit 3)
 if [[ ! -z "$output" ]]; then
