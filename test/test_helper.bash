60c66e65f3a67056363f823ab4364ecfe90287bb
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-11-15 19:49:33 +0100

    Add stub function

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,10 +1,17 @@
 setup() {
   export TMP="$BATS_TEST_DIRNAME/tmp"
   export NAGIOS_PLUGINS_DIRECTORY="$TMP/../.."
-  export PATH="$NAGIOS_PLUGINS_DIRECTORY:$TMP/bin:$PATH"
-  export NAGIOS_BASH_SCRIPTS=$(find "$NAGIOS_PLUGINS_DIRECTORY" -type f -name '*.sh' -print)
+  export PATH="$NAGIOS_PLUGINS_DIRECTORY:$TMP/stub:$PATH"
+  export NAGIOS_BASH_SCRIPTS=$(find "$NAGIOS_PLUGINS_DIRECTORY" -type f -maxdepth 1 -name '*.sh' -print)
 }
 
 teardown() {
-  [ -d "$TMP" ] && rm -f "$TMP"/*
+  [[ -d "$TMP" ]] && rm -rf "$TMP"/*
+}
+
+# http://www.nherson.com/blog/2014/01/13/stubbing-system-executables-with-bats
+stub() {
+  mkdir -p "$BATS_TEST_DIRNAME/tmp/stub"
+  echo "echo '$2'" > "$BATS_TEST_DIRNAME/tmp/stub/$1"
+  chmod +x "$BATS_TEST_DIRNAME/tmp/stub/$1"
 }
