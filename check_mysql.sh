fb03d61151e8a21607c388900e78512371e7376a
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2015-04-10 22:56:31 +0200

    Improve check_mysql in order to check max_connections threshold
    Too many idle connected_thread result to 'Too many connections' errors
    client side.

--- check_mysql.sh
+++ check_mysql.sh
@@ -6,6 +6,8 @@
 #   -u, --user                  MySQL user name
 #   -p, --port                  MySQL user password
 #   -f, --defaults-file         MySQL defaults-file path
+#   -w, --warning WARNING       Warning value (percent)
+#   -c, --critical CRITICAL     Critical value (percent)
 #   -h, --help                  Display this screen
 #
 # (c) 2014, Benjamin Dos Santos <benjamin.dossantos@gmail.com>
@@ -24,6 +26,14 @@ while [[ -n "$1" ]]; do
       ;;
     --defaults-file | -f)
       default_files=$2
+      shift
+      ;;
+    --warning | -w)
+      warning=$2
+      shift
+      ;;
+    --critical | -c)
+      critical=$2
       shift
       ;;
     --help | -h)
@@ -45,6 +55,13 @@ if ! hash mysqladmin &>/dev/null; then
 fi
 
 options=()
+warning=${warning:=90}
+critical=${critical:=95}
+
+if [[ $warning -ge $critical ]]; then
+  echo "UNKNOWN - warning ($warning) can't be greater than critical ($critical)"
+  exit 3
+fi
 
 if [[ ! -z $user ]]; then
   options=("${options[@]}" "-u ${user}")
@@ -59,7 +76,33 @@ if [[ ! -z $default_files ]]; then
 fi
 
 status=$(mysqladmin "${options[@]}" status)
-exit_code=$?
+if [[ $? -ne 0 ]] || [[ -z $status ]]; then
+  echo "CRITICAL - ${status}"
+  exit 2
+fi
 
-echo "$status"
-exit $exit_code
+connected_thread=$(echo "$status" | awk '{ print $4 };')
+max_connections=$(mysql -N -s -r -e 'SHOW VARIABLES LIKE "max_connections";' | \
+  awk '{ print $2 };'
+)
+
+if [[ $? -ne 0 ]]; then
+  echo "CRITICAL - could not fetch MySQL max_connections"
+  exit 2
+elif [[ -z $connected_thread ]] || [[ -z $max_connections ]]; then
+  echo "CRITICAL - 'connected_thread' and 'max_connections' are empty"
+  exit 2
+fi
+
+used=$((connected_thread * 100 / max_connections))
+status="${status} - ${connected_thread} of ${max_connections} max_connections";
+if [[ $used -gt $critical ]]; then
+  echo "CRITICAL - ${status}"
+  exit 2
+elif [[ $used -gt $warning ]]; then
+  echo "WARNING - ${status}"
+  exit 1
+else
+  echo "OK - ${status}"
+  exit 0
+fi
--- test/check_mysql.bats
+++ test/check_mysql.bats
@@ -18,9 +18,42 @@ load test_helper
   [ "$status" -eq 2 ]
 }
 
-@test 'Test check_mysql.sh output with fake mysqladmin command and successfull result' {
+@test "Test check_mysql.sh when can't fetch 'max_connections' value" {
   local output='Uptime: 1232296  Threads: 7  Questions: 148586347  Slow queries: 965  Opens: 11911  Flush tables: 2  Open tables: 256  Queries per second avg: 120.576'
   stub mysqladmin "$output"
+  stub mysql 'error' 1
+  stub awk 'error' 1
+
+  run check_mysql.sh
+  echo "$output" >/tmp/my
+  [ "$status" -eq 2 ]
+  echo "$output" | grep 'CRITICAL - could not fetch MySQL max_connections'
+}
+
+@test "Test check_mysql.sh when 'connected_thread' and 'max_connections' are empty" {
+  local output='Uptime: 1232296  Threads: 7  Questions: 148586347  Slow queries: 965  Opens: 11911  Flush tables: 2  Open tables: 256  Queries per second avg: 120.576'
+  stub mysqladmin "$output"
+  stub mysql '' 1
+
+  run check_mysql.sh
+  [ "$status" -eq 2 ]
+  echo "$output" | grep "CRITICAL - 'connected_thread' and 'max_connections' are empty"
+}
+
+@test 'Test check_mysql.sh with warn flag greater than critical' {
+  local output='Uptime: 1232296  Threads: 7  Questions: 148586347  Slow queries: 965  Opens: 11911  Flush tables: 2  Open tables: 256  Queries per second avg: 120.576'
+  stub mysqladmin "$output"
+
+  run check_mysql.sh --warning 95 --critical 90
+  [ "$status" -eq 3 ]
+  echo "$output" >/tmp/my
+  echo "$output" | grep "UNKNOWN - warning (95) can't be greater than critical (90)"
+}
+
+@test 'Test check_mysql.sh output with fake mysqladmin command and successfull result' {
+  local output='Uptime: 1232296  Threads: 7  Questions: 148586347  Slow queries: 965  Opens: 11911  Flush tables: 2  Open tables: 256  Queries per second avg: 120.576 - 7 of 500 max_connections'
+  stub mysqladmin "$output"
+  stub mysql 'max_connections 500'
 
   run check_mysql.sh
   [ "$status" -eq 0 ]
