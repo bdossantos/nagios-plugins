a533d58035481a6baaa4dbdd8cd39c1c243b6b25
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2015-04-12 22:43:19 +0200

    Check if network interface statistics are fetchable
    And rename some variables for concistency

--- check_bandwidth.sh
+++ check_bandwidth.sh
@@ -24,11 +24,11 @@ while [[ -n "$1" ]]; do
       shift
       ;;
     --warning | -w)
-      warn=$2
+      warning=$2
       shift
       ;;
     --critical | -c)
-      crit=$2
+      critical=$2
       shift
       ;;
     --help | -h)
@@ -44,10 +44,21 @@ while [[ -n "$1" ]]; do
   shift
 done
 
-warn=${warn:=5000}
-crit=${crit:=6000}
+warning=${warning:=5000}
+critical=${critical:=8000}
 interface=${interface:=eth0}
-sleep=${sleep:=5}
+sleep=${sleep:=1}
+
+if [[ $warning -ge $critical ]]; then
+  echo "UNKNOWN - warning ($warning) can't be greater than critical ($critical)"
+  exit 3
+fi
+
+if [[ ! -f "/sys/class/net/${interface}/statistics/rx_bytes" ]] ||
+  [[ ! -f "/sys/class/net/${interface}/statistics/tx_bytes" ]]; then
+  echo "CRITICAL - Could not fetch '${interface}' interface statistics"
+  exit 2
+fi
 
 rx1=$(cat "/sys/class/net/${interface}/statistics/rx_bytes")
 tx1=$(cat "/sys/class/net/${interface}/statistics/tx_bytes")
@@ -60,9 +71,9 @@ rx_bps=$((rx2 - rx1))
 tx_kbps=$((tx_bps / 1024))
 rx_kbps=$((rx_bps / 1024))
 
-status="tx $1: $tx_kbps kb/s rx $1: $rx_kbps kb/s"
-if [[ $rx_kbps -ge $warn ]] || [[ $tx_kbps -ge $warn ]]; then
-  if [[ $rx_kbps -ge $crit ]] || [[ $tx_kbps -ge $crit ]]; then
+status="tx $1: $tx_kbps kb/s | rx $1: $rx_kbps kb/s"
+if [[ $rx_kbps -ge $warning ]] || [[ $tx_kbps -ge $warning ]]; then
+  if [[ $rx_kbps -ge $critical ]] || [[ $tx_kbps -ge $critical ]]; then
     exit_status=2
     echo "CRITICAL - $status"
   else
