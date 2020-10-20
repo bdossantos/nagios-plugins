275140d7fd2659dd0babf53826996dd64b7b3d79
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2020-10-20 11:43:04 +0200

    chore: add 'critical medium error' to checklist

--- check_dmesg.sh
+++ check_dmesg.sh
@@ -11,15 +11,15 @@
 
 while [[ -n "$1" ]]; do
   case $1 in
-    --help | -h)
-      sed -n '2,7p' "$0" | tr -d '#'
-      exit 3
-      ;;
-    *)
-      echo "Unknown argument: $1"
-      exec "$0" --help
-      exit 3
-      ;;
+  --help | -h)
+    sed -n '2,7p' "$0" | tr -d '#'
+    exit 3
+    ;;
+  *)
+    echo "Unknown argument: $1"
+    exec "$0" --help
+    exit 3
+    ;;
   esac
   shift
 done
@@ -28,7 +28,7 @@ output=$(dmesg -T -l warn,err,crit,alert,emerg 2>/dev/null || dmesg || exit 3)
 if [[ ! -z "$output" ]]; then
   filtered_output=$({
     echo "$output" | egrep -o -i \
-    'Hardware Error|I/O error|hard resetting link|DRDY ERR|Out of memory|Killed process|temperature above threshold|Possible SYN flooding|segfault|MEMORY ERROR|dropping packet'
+      'Hardware Error|I/O error|hard resetting link|DRDY ERR|critical medium error|Out of memory|Killed process|temperature above threshold|Possible SYN flooding|segfault|MEMORY ERROR|dropping packet'
   })
 
   if [[ ! -z "$filtered_output" ]]; then
